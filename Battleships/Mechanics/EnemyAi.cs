6477f6be4b45c99725a8f1add8cace92416e6e90
orzech <michalorzechowski123@gmail.com>
2019-09-14 00:45:41 +0200

    Created EnemyAi class

---
+++ Battleships/Mechanics/EnemyAi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Battleships.Components;
+
+namespace Battleships.Mechanics
+{
+    public class EnemyAi
+    {
+        private readonly Random _rnd = new Random(Guid.NewGuid().GetHashCode());
+
+        public (int Col, int Row) GetNextBestShot(BoardComponent boardComponent)
+        {
+            return (_rnd.Next(0, boardComponent.Board.Cols), _rnd.Next(0, boardComponent.Board.Rows));
+        }
+    }
+}
